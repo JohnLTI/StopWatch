c74d0b4e41f0079c93b7518738f74fbf7b8d5355
JohnLTI <xjl16@outlook.com>
2023-07-17 22:53:18 -0300

    Ajustes Função Menu

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -7,18 +7,22 @@ namespace StopWatch
     {
         static void Main(string[] args)
         {
-            start(6);
+            menu();
         }
 
         static void menu()
         {
             Console.Clear();
+            Console.WriteLine("-----Seja Bem-Vindo!-----");
+            Console.WriteLine("");
             Console.WriteLine("S = Segundo => 10s = 10 Segundos");
             Console.WriteLine("M = Minuto => 1m = 1 Minuto");
             Console.WriteLine("0 = Sair");
+            Console.WriteLine("");
             Console.WriteLine("Quanto tempo deseja contar?");
 
             string data = Console.ReadLine().ToLower(); // CONVERSÃO PARA STRING E COM FUNÇÃO TOLOWER QUE CONVERTE TUDO PARA MINÚSCULO
+
             char type = char.Parse(data.Substring(data.Length - 1, 1)); //DATA.LENGTH CONTA OS NÚMEROS DE CARACTERES QUE POSSUI O TEXTO INFORMADO
             int time = int.Parse(data.Substring(0, data.Length - 1));
             int multiply = 1;
